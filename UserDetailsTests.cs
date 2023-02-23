7a13e1a0e02651c46bf02bcdbeddd37f995375f9
blue-acer <110229245+blue-acer@users.noreply.github.com>
2023-02-23 12:50:02 +0000

    Performed first 2 tests successfully

--- UserDetailsTests.cs
+++ UserDetailsTests.cs
@@ -1,6 +1,6 @@
-using FakeItEasy;
-using Microsoft.EntityFrameworkCore;
-using UserAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using UserAPI.Controllers;
 using UserAPI.Interfaces;
 using UserAPI.Models;
 
@@ -8,18 +8,72 @@ namespace UserAPI.Test
 {
     public class UserDetailsTests
     {
-        private readonly IUserDetailsRepository _userDetailsRepository;
+        //private readonly IUserDetailsRepository _userDetailsRepository;
 
-        public UserDetailsTests()
+        [Fact]
+        public async Task UserDetailsController_GetAllUserDetails_ReturnOKAsync()
         {
-            _userDetailsRepository = A.Fake<IUserDetailsRepository>();
+            //Arrange
+            var mockInterface = new Mock<IUserDetailsRepository>();
+
+            var user1 = new UserDetails();
+            user1.FirstName = "Jane";
+            user1.LastName = "Doe";
+            user1.EmailAddress = "jane.doe@test.com";
+            var user2 = new UserDetails();
+            user2.FirstName = "John";
+            user2.LastName = "Smith";
+            user2.EmailAddress = "j.smith@test.com";
+
+            mockInterface.Setup(x =>
+            x.GetAllUserDetailsData()).ReturnsAsync(
+                new List<UserDetails>
+                {
+                user1,
+                user2
+                });
+
+            var controller = new UserDetailsController(mockInterface.Object);
+
+            //Act
+            var usersResult = await controller.GetAllUserDetails();
+
+            //Assert
+            var result = usersResult.Result as OkObjectResult;
+            Assert.NotNull(result);
         }
 
         [Fact]
-        public void Test1()
+        public async Task UserDetailsController_GetAllUsersSP_ReturnOKAsync()
         {
-            
+            //Arrange
+            var mockInterface = new Mock<IUserDetailsRepository>();
 
+            var user1 = new UserDetails();
+            user1.FirstName = "Jane";
+            user1.LastName = "Doe";
+            user1.EmailAddress = "jane.doe@test.com";
+            var user2 = new UserDetails();
+            user2.FirstName = "John";
+            user2.LastName = "Smith";
+            user2.EmailAddress = "j.smith@test.com";
+
+            mockInterface.Setup(x =>
+            x.GetAllUserDetailsSPData()).ReturnsAsync(
+                new List<UserDetails>
+                {
+                user1,
+                user2
+                });
+
+            var controller = new UserDetailsController(mockInterface.Object);
+
+            //Act
+            var usersResult = await controller.GetAllUserDetailsSP();
+
+            //Assert
+            var result = usersResult.Result as OkObjectResult;
+            Assert.NotNull(result);
         }
     }
 }
